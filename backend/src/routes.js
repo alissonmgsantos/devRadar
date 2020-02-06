3725abb4a6894eeadac59f926b7d0315d9d3fd37
Alisson Matos <alissonmgsantos@gmail.com>
2020-02-06 20:44:50 -0300

    Cadastrando Dev e consumindo api GitHub

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
-
+const axios = require('axios');
+const Dev = require('./models/Dev');
 // Methods HTTP: GET, POST, PUT, DELETE
 
 // Query Params: req.body (Filtros, ordenação, paginação)
@@ -7,8 +8,23 @@ const { Router } = require('express');
 // Body: req.body (Dados para criação ou alteração de algum registro)
 
 const routes = Router();
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Wolrd' });
+routes.post('/dev', async (req, res) => {
+  const { github_username, techs } = req.body;
+  const result = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+  const { name = login, avatar_url, bio } = result.data;
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray
+  });
+
+  res.json(dev);
 });
 
 module.exports = routes;
